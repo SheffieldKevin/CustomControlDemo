5d27254f92de55cd1d1f1e9a927bf5c4d55c4da9
Kevin Meaney <ktam@yvs.eu.com>
2015-07-25 09:00:47 +0100

    Updating Framework for custom control demo.

--- DerivedData/CustomControlDemo/Build/Products/Debug-iphoneos/MovingImagesiOS.framework/Headers/MovingImagesiOS.h
+++ DerivedData/CustomControlDemo/Build/Products/Debug-iphoneos/MovingImagesiOS.framework/Headers/MovingImagesiOS.h
@@ -12,7 +12,6 @@ FOUNDATION_EXPORT double MovingImagesFrameworkiOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char MovingImagesFrameworkiOSVersionString[];
 
 #import <MovingImagesiOS/MIHandleCommands.h>
-#import <MovingImagesiOS/MICGImage.h>
 #import <MovingImagesiOS/MIConstants.h>
 #import <MovingImagesiOS/MIContext.h>
 #import <MovingImagesiOS/MICoreImageJSONConstants.h>
